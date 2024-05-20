85020ee978e93f34ecb7e830aa262b93dcefa791
Teodor Vidakovic <teodorv22@gmail.com>
2024-05-20 11:23:19 +0200

    feat: add simulation driver

---
+++ SCADA-Core/SCADA-Core/Drivers/SimulationDriver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScadaPlus.Drivers
+{
+    public class SimulationDriver
+    {
+        public double GetValue(string address)
+        {
+            return address switch
+            {
+                "S" => Sine(),
+                "C" => Cosine(),
+                "R" => Ramp(),
+                _ => -1000,
+            };
+        }
+
+        private double Sine()
+        {
+            return 100 * Math.Sin((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private double Cosine()
+        {
+            return 100 * Math.Cos((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private double Ramp()
+        {
+            return 100 * DateTime.Now.Second / 60;
+        }
+    }
+}
