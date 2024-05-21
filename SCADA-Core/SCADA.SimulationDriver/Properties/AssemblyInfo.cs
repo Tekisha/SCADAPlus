a7ba4f17b953a9dc57d9d3fae84efed82b9c0b64
Teodor Vidakovic <teodorv22@gmail.com>
2024-05-21 13:59:11 +0200

    feat: change namespace in assemblyinfo.cs

--- SCADA-Core/SCADA.SimulationDriver/Properties/AssemblyInfo.cs
+++ SCADA-Core/SCADA.SimulationDriver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SCADA.SimulationDriver")]
+[assembly: AssemblyTitle("SCADA_Driver.SimulationDriver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SCADA.SimulationDriver")]
+[assembly: AssemblyProduct("SCADA_Driver.SimulationDriver")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
