243cef755ddfadf922985ef0bc10036ce6195930
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-01 22:01:23 +0200

    feat: remove comments

--- DatabaseManager/DatabaseManager/Program.cs
+++ DatabaseManager/DatabaseManager/Program.cs
@@ -13,28 +13,20 @@ namespace DatabaseManager
     {
         static void Main(string[] args)
         {
-            // Base addresses for the services
             string tagServiceBaseAddress = "http://localhost:8733/SCADA/TagController/";
             string userServiceBaseAddress = "http://localhost:8733/SCADA/UserController/";
 
-            // Create bindings
             var binding = new BasicHttpBinding();
 
-            // Create endpoints
             var tagServiceEndpoint = new EndpointAddress(tagServiceBaseAddress);
             var userServiceEndpoint = new EndpointAddress(userServiceBaseAddress);
 
-            // Create channel factories
             var tagServiceFactory = new ChannelFactory<ITagController>(binding, tagServiceEndpoint);
             var userServiceFactory = new ChannelFactory<IUserController>(binding, userServiceEndpoint);
 
-            // Create service proxies
             ITagController tagServiceProxy = tagServiceFactory.CreateChannel();
             IUserController userServiceProxy = userServiceFactory.CreateChannel();
 
-            // Use the proxies to call the service operations
-
-            // Example: Add a new tag
             var newTag = new AnalogInputTag
             {
                 Id = "tagId1",
@@ -52,11 +44,9 @@ namespace DatabaseManager
             tagServiceProxy.AddTag(newTag);
             Console.WriteLine("Added new tag.");
 
-            // Example: Get the value of a tag
             double tagValue = tagServiceProxy.GetTagValue("tagId1");
             Console.WriteLine($"Tag Value: {tagValue}");
 
-            // Example: Register a new user
             var newUser = new User
             {
                 Username = "testuser",
@@ -66,15 +56,12 @@ namespace DatabaseManager
             userServiceProxy.RegisterUser(newUser);
             Console.WriteLine("Registered new user.");
 
-            // Example: Validate user credentials
             bool isValidUser = userServiceProxy.ValidateUser("testuser", "password123");
             Console.WriteLine($"User validation result: {isValidUser}");
 
-            // Close the proxies
             ((IClientChannel)tagServiceProxy).Close();
             ((IClientChannel)userServiceProxy).Close();
 
-            // Close the factories
             tagServiceFactory.Close();
             userServiceFactory.Close();
 
