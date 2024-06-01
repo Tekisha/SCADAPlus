705883a562699a6b43d6a07c4fe50e3f500b92d3
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-01 22:00:59 +0200

    feat: create SimulationDriver library class

---
+++ SimulationDriver/SimulationDriver/SimulationDriver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationDriver
+{
+    public static class SimulationDriver
+    {
+        public static double GetValue(string address)
+        {
+            return address switch
+            {
+                "S" => Sine(),
+                "C" => Cosine(),
+                "R" => Ramp(),
+                _ => -1000,
+            };
+        }
+
+        private static double Sine()
+        {
+            return 100 * Math.Sin((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private static double Cosine()
+        {
+            return 100 * Math.Cos((double)DateTime.Now.Second / 60 * Math.PI);
+        }
+
+        private static double Ramp()
+        {
+            return 100 * DateTime.Now.Second / 60;
+        }
+    }
+}
