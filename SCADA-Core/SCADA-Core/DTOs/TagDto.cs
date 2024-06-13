1c67b05a3ffb9c13efc5cd2f299ecfd500228d75
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-13 02:50:40 +0200

    feat: Create Tag Dto which implements Data Contract

---
+++ SCADA-Core/SCADA-Core/DTOs/TagDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace SCADA_Core.DTOs;
+
+[DataContract]
+public class TagDto
+{
+    [DataMember] public string Id { get; set; }
+
+    [DataMember] public string Description { get; set; }
+
+    [DataMember] public string IoAddress { get; set; }
+
+    [DataMember] public string Driver { get; set; }
+
+    [DataMember] public int ScanTime { get; set; }
+
+    [DataMember] public bool OnOffScan { get; set; }
+
+    [DataMember] public double LowLimit { get; set; }
+
+    [DataMember] public double HighLimit { get; set; }
+
+    [DataMember] public string Units { get; set; }
+
+    [DataMember] public bool Alarms { get; set; }
+}
