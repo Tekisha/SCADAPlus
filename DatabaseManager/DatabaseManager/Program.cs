f6dce2f9badd918e8e074b3ede2844b7bde8ef56
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-13 03:34:56 +0200

    feat: Cleanup main and configure project version

--- DatabaseManager/DatabaseManager/Program.cs
+++ DatabaseManager/DatabaseManager/Program.cs
@@ -1,54 +1,48 @@
-﻿using DatabaseManager.TagService;
-using DatabaseManager.UserService;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
+using DatabaseManager.TagService;
+using DatabaseManager.UserService;
 
-namespace DatabaseManager
+namespace DatabaseManager;
+
+internal class Program
 {
-    class Program
+    private static void Main()
     {
-        static void Main(string[] args)
-        {
-            string tagServiceBaseAddress = "http://localhost:8733/SCADA/TagController/";
-            string userServiceBaseAddress = "http://localhost:8733/SCADA/UserController/";
+        const string tagServiceBaseAddress = "http://localhost:8733/SCADA/TagController/";
+        const string userServiceBaseAddress = "http://localhost:8733/SCADA/UserController/";
 
-            var binding = new BasicHttpBinding();
+        var binding = new BasicHttpBinding();
 
-            var tagServiceEndpoint = new EndpointAddress(tagServiceBaseAddress);
-            var userServiceEndpoint = new EndpointAddress(userServiceBaseAddress);
+        var tagServiceEndpoint = new EndpointAddress(tagServiceBaseAddress);
+        var userServiceEndpoint = new EndpointAddress(userServiceBaseAddress);
 
-            var tagServiceFactory = new ChannelFactory<ITagController>(binding, tagServiceEndpoint);
-            var userServiceFactory = new ChannelFactory<IUserController>(binding, userServiceEndpoint);
+        var tagServiceFactory = new ChannelFactory<ITagController>(binding, tagServiceEndpoint);
+        var userServiceFactory = new ChannelFactory<IUserController>(binding, userServiceEndpoint);
 
-            ITagController tagServiceProxy = tagServiceFactory.CreateChannel();
-            IUserController userServiceProxy = userServiceFactory.CreateChannel();
+        var tagServiceProxy = tagServiceFactory.CreateChannel();
+        var userServiceProxy = userServiceFactory.CreateChannel();
 
-            tagServiceProxy.AddTag("tagId1", "Test Analog Input Tag", "AI1", "DefaultDriver", 1000, true, 0, 100, "Celsius", true);
-            Console.WriteLine("Added new tag.");
+        tagServiceProxy.AddTag("tagId1", "Test Analog Input Tag", "AI1", "DefaultDriver", 1000, true, 0, 100,
+            "Celsius", true);
+        Console.WriteLine("Added new tag.");
 
-            double tagValue = tagServiceProxy.GetTagValue("tagId1");
-            Console.WriteLine($"Tag Value: {tagValue}");
+        var tagValue = tagServiceProxy.GetTagValue("tagId1");
+        Console.WriteLine($"Tag Value: {tagValue}");
 
-            userServiceProxy.RegisterUser("testuser", "password123");
-            Console.WriteLine("Registered new user.");
+        userServiceProxy.RegisterUser("testuser", "password123");
+        Console.WriteLine("Registered new user.");
 
-            bool isLoggedIn = userServiceProxy.LogIn("testuser", "password123");
-            Console.WriteLine($"User validation result: {isLoggedIn}");
+        var isLoggedIn = userServiceProxy.LogIn("testuser", "password123");
+        Console.WriteLine($"User validation result: {isLoggedIn}");
 
-            ((IClientChannel)tagServiceProxy).Close();
-            ((IClientChannel)userServiceProxy).Close();
+        ((IClientChannel)tagServiceProxy).Close();
+        ((IClientChannel)userServiceProxy).Close();
 
-            tagServiceFactory.Close();
-            userServiceFactory.Close();
+        tagServiceFactory.Close();
+        userServiceFactory.Close();
 
-            Console.WriteLine("Press [Enter] to exit.");
-            Console.ReadLine();
-        }
+        Console.WriteLine("Press [Enter] to exit.");
+        Console.ReadLine();
     }
 }
