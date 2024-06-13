0b2a523db8f7ce7de8783eb2003b940e0555fdee
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-13 03:28:24 +0200

    feat: Simplify user controller

--- SCADA-Core/SCADA-Core/Controllers/implementations/UserController.cs
+++ SCADA-Core/SCADA-Core/Controllers/implementations/UserController.cs
@@ -3,15 +3,8 @@ using SCADA_Core.Services.interfaces;
 
 namespace SCADA_Core.Controllers.implementations;
 
-public class UserController : IUserController
+public class UserController(IUserService userService) : IUserController
 {
-    private readonly IUserService userService;
-
-    public UserController(IUserService userService)
-    {
-        this.userService = userService;
-    }
-
     public bool RegisterUser(string username, string password)
     {
         return userService.RegisterUser(username, password);
