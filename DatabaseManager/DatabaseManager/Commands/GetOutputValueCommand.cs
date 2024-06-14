1513f0ab697b5cd0f58d5f8873bc69072ae3ceb1
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-14 15:03:27 +0200

    feat: Add GetOutputValueCommand functionality
    - Introduced a new file `GetOutputValueCommand.cs` under the `Commands` directory in `DatabaseManager.csproj`, marking the addition of a new command to the application.
    - Implemented the `GetOutputValueCommand` class within the `DatabaseManager.Commands` namespace, adhering to the `ICommand` interface, to facilitate retrieving the output value of a tag.
    - Added necessary `using` directives for `System` and `DatabaseManager.TagService` to support the command's functionality.
    - The command's constructor accepts an `ITagController`, enabling interaction with tags.
    - Defined `GetDescription` method to return a human-readable command description: "Get Tag Output Value."
    - Implemented the `Execute` method to prompt user input for a tag ID, retrieve, and display the tag's value using the provided `tagController`.

---
+++ DatabaseManager/DatabaseManager/Commands/GetOutputValueCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DatabaseManager.TagService;
+
+namespace DatabaseManager.Commands;
+
+internal class GetOutputValueCommand(ITagController tagController) : ICommand
+{
+    public string GetDescription()
+    {
+        return "Get Tag Output Value.";
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("Enter tag ID:");
+        var tagId = Console.ReadLine();
+
+        var tagValue = tagController.GetTagValue(tagId);
+        Console.WriteLine($"Tag Value: {tagValue}");
+    }
+}
