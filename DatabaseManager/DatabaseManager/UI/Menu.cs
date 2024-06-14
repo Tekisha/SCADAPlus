f2c29dc6e1687814e95a9fc615151c1d2481b135
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-14 14:58:34 +0200

    feat: Add Menu class with Show method to DatabaseManager.UI
    - Introduced a new `Menu` class in the `DatabaseManager.UI` namespace.
    - Added a `using` directive for the `System` namespace.
    - Implemented a public static `Show` method in the `Menu` class to print a console menu.
    - The `Show` method includes options for tag management, user registration, login, and exit.
    - Noted an incomplete class definition due to a dangling `private` keyword.

---
+++ DatabaseManager/DatabaseManager/UI/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatabaseManager.UI;
+
+public class Menu
+{
+    public static void Show()
+    {
+        Console.WriteLine("1. Add new tag.");
+        Console.WriteLine("2. Get tag value.");
+        Console.WriteLine("3. Register new user.");
+        Console.WriteLine("4. Log in.");
+        Console.WriteLine("5. Exit.");
+    }
+
+    private 
+}
