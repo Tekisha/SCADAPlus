a6d4f89a9e2fba4e6298cb825b59e195a7445c7f
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-14 16:24:39 +0200

    feat: Refactor Program.cs for modularity and UX
    - Refactored `Program.cs` to enhance modularity by introducing a `Menu` class for user interactions and command execution, improving code organization and readability.
    - Removed direct service calls from `Program.cs`, delegating service interaction to the `Menu` class, which now handles user commands through a dynamic interface.
    - Implemented the Command pattern in `Menu.cs` to encapsulate user requests, allowing for flexible command execution and future expansion. This includes a dynamic user interface for command selection and execution flow control.
    - Adjusted namespaces and `using` directives to reflect the new structure, with `Program.cs` focusing on initialization and `Menu.cs` managing user interactions and service calls.
    This comprehensive refactor enhances the DatabaseManager application's architecture, maintainability, and user experience.

--- DatabaseManager/DatabaseManager/Program.cs
+++ DatabaseManager/DatabaseManager/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.ServiceModel;
-using DatabaseManager.TagService;
-using DatabaseManager.UserService;
+﻿using DatabaseManager.UI;
 
 namespace DatabaseManager;
 
@@ -9,53 +6,7 @@ internal class Program
 {
     private static void Main()
     {
-        const string tagServiceBaseAddress = "http://localhost:8733/SCADA/TagController/";
-        const string userServiceBaseAddress = "http://localhost:8733/SCADA/UserController/";
-
-        var binding = new BasicHttpBinding();
-
-        var tagServiceEndpoint = new EndpointAddress(tagServiceBaseAddress);
-        var userServiceEndpoint = new EndpointAddress(userServiceBaseAddress);
-
-        var tagServiceFactory = new ChannelFactory<ITagController>(binding, tagServiceEndpoint);
-        var userServiceFactory = new ChannelFactory<IUserController>(binding, userServiceEndpoint);
-
-        var tagServiceProxy = tagServiceFactory.CreateChannel();
-        var userServiceProxy = userServiceFactory.CreateChannel();
-
-        var tagDto = new TagDto
-        {
-            Id = "tagId1",
-            Description = "Test Analog Input Tag",
-            IoAddress = "AI1",
-            Driver = "DefaultDriver",
-            ScanTime = 1000,
-            OnOffScan = true,
-            LowLimit = 0,
-            HighLimit = 100,
-            Units = "Celsius",
-            Alarms = true
-        };
-
-        tagServiceProxy.AddTag(tagDto);
-        Console.WriteLine("Added new tag.");
-
-        var tagValue = tagServiceProxy.GetTagValue("tagId1");
-        Console.WriteLine($"Tag Value: {tagValue}");
-
-        userServiceProxy.RegisterUser("testuser", "password123");
-        Console.WriteLine("Registered new user.");
-
-        var isLoggedIn = userServiceProxy.LogIn("testuser", "password123");
-        Console.WriteLine($"User validation result: {isLoggedIn}");
-
-        ((IClientChannel)tagServiceProxy).Close();
-        ((IClientChannel)userServiceProxy).Close();
-
-        tagServiceFactory.Close();
-        userServiceFactory.Close();
-
-        Console.WriteLine("Press [Enter] to exit.");
-        Console.ReadLine();
+        var menu = new Menu();
+        menu.Run();
     }
 }
--- DatabaseManager/DatabaseManager/UI/Menu.cs
+++ DatabaseManager/DatabaseManager/UI/Menu.cs
@@ -1,15 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using DatabaseManager.Commands;
+using DatabaseManager.TagService;
+using DatabaseManager.UserService;
 
 namespace DatabaseManager.UI;
 
 public class Menu
 {
-    public static void Show()
+    private readonly Dictionary<int, ICommand> _commands;
+
+    public Menu()
     {
-        Console.WriteLine("1. Add new tag.");
-        Console.WriteLine("2. Get tag value.");
-        Console.WriteLine("3. Register new user.");
-        Console.WriteLine("4. Log in.");
-        Console.WriteLine("5. Exit.");
+        const string tagServiceBaseAddress = "http://localhost:8733/SCADA/TagController/";
+        const string userServiceBaseAddress = "http://localhost:8733/SCADA/UserController/";
+
+        var binding = new BasicHttpBinding();
+
+        var tagServiceEndpoint = new EndpointAddress(tagServiceBaseAddress);
+        var userServiceEndpoint = new EndpointAddress(userServiceBaseAddress);
+
+        var tagServiceFactory = new ChannelFactory<ITagController>(binding, tagServiceEndpoint);
+        var userServiceFactory = new ChannelFactory<IUserController>(binding, userServiceEndpoint);
+
+        var tagServiceProxy = tagServiceFactory.CreateChannel();
+        var userServiceProxy = userServiceFactory.CreateChannel();
+
+        _commands = new Dictionary<int, ICommand>
+        {
+            //{1, new AddTagCommand(tagServiceProxy)},
+            { 2, new GetOutputValueCommand(tagServiceProxy) },
+            { 3, new GetAllTagsCommand(tagServiceProxy) },
+            //{4, new RegisterUserCommand(userServiceProxy)},
+            { 5, new LogInCommand(userServiceProxy) },
+            //{6, new LogOutCommand(userServiceProxy)},
+            { 7, new ExitCommand() }
+        };
+    }
+
+    private void Show()
+    {
+        foreach (var command in _commands) Console.WriteLine($"{command.Key} - {command.Value.GetDescription()}");
+    }
+
+    private ICommand GetCommand()
+    {
+        int command;
+        Console.Write("Enter command: ");
+        while (!int.TryParse(Console.ReadLine(), out command) || !_commands.ContainsKey(command))
+            Console.Write("Invalid command. Please try again. Enter command: ");
+
+        return _commands[command];
+    }
+
+    public void Run()
+    {
+        ICommand command;
+        do
+        {
+            Show();
+            command = GetCommand();
+            command.Execute();
+        } while (command is not ExitCommand);
     }
 }
