d00315ff156d2289510b55a9f2ba7ad0621f3101
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-14 14:59:58 +0200

    feat: Added command pattern and user login logic
    - Updated DatabaseManager.csproj to include new command pattern files and a user interface component.
    - Added `ExitCommand.cs` implementing `ICommand` for application exit functionality.
    - Created `ICommand.cs` interface to define command execution and description methods.
    - Implemented `LogInCommand.cs` for user login handling, utilizing `UserController` for authentication.

---
+++ DatabaseManager/DatabaseManager/Commands/ExitCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DatabaseManager.Commands;
+
+internal class ExitCommand : ICommand
+{
+    public string GetDescription()
+    {
+        return "Exit application.";
+    }
+
+    public void Execute()
+    {
+        Environment.Exit(0);
+    }
+}
