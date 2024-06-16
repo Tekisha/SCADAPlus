4d2ec872051b9b8dde53350c9f8bc26b00e7e8ad
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-16 14:18:07 +0200

    feat: implement initial alarm controller

---
+++ SCADA-Core/SCADA-Core/Controllers/implementations/AlarmController.cs
@@ -0,0 +1,50 @@
+﻿using SCADA_Core.Controllers.interfaces;
+using SCADA_Core.DTOs;
+using SCADA_Core.Models;
+using SCADA_Core.Services.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA_Core.Controllers.implementations
+{
+    public class AlarmController(IAlarmService alarmService) : IAlarmController
+    {
+        public IEnumerable<AlarmDto> GetAll()
+        {
+            return alarmService.GetAll();
+        }
+
+        public AlarmDto Get(string alarmName)
+        {
+            return alarmService.Get(alarmName);
+        }
+
+        public IEnumerable<AlarmDto> GetInvoked(string tagId, double value)
+        {
+            return alarmService.GetInvoked(tagId, value);
+        }
+
+        public AlarmDto Create(AlarmDto newAlarm)
+        {
+            return alarmService.Create(newAlarm);
+        }
+
+        public AlarmDto Delete(string alarmName)
+        {
+            return alarmService.Delete(alarmName);
+        }
+
+        public AlarmDto Update(AlarmDto updatedAlarm)
+        {
+            return alarmService.Update(updatedAlarm);
+        }
+
+        public IEnumerable<AlarmDto> GetByTag(string tagId)
+        {
+            return alarmService.GetByTag(tagId);
+        }
+    }
+}
