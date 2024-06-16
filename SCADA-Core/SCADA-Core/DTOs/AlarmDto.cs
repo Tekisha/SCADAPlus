0b08d1b4054dda1ed4f34b4aa7bb29979e25d3db
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-16 10:59:00 +0200

    feat: create initial alarm model

---
+++ SCADA-Core/SCADA-Core/DTOs/AlarmDto.cs
@@ -0,0 +1,33 @@
+﻿using SCADA_Core.Enums;
+using SCADA_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA_Core.DTOs
+{
+    public class AlarmDto
+    {
+        public string AlarmName { get; set; }
+        public string TagName { get; set; }
+        public double Limit { get; set; }
+        public string Type { get; set; } // "ABOVE" or "BELOW"
+        public AlarmPriority Priority { get; set; }
+        public bool Acknowledged { get; set; }
+
+        public Alarm ToEntity()
+        {
+            return new Alarm
+            {
+                AlarmName = AlarmName,
+                TagName = TagName,
+                Limit = Limit,
+                Type = Type == "ABOVE" ? AlarmType.Above : AlarmType.Below,
+                Priority = Priority,
+                Acknowledged = Acknowledged
+            };
+        }
+    }
+}
---
+++ SCADA-Core/SCADA-Core/Models/Alarm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA_Core.Models
+{
+    public class Alarm
+    {
+        public string Id { get; set; }
+        public string AlarmName { get; set; }
+        public string TagName { get; set; }
+        public double Limit { get; set; }
+        public AlarmType Type { get; set; } 
+        public AlarmPriority Priority { get; set; }
+        public bool Acknowledged { get; set; }
+    }
+
+    public enum AlarmType
+    {
+        Above,
+        Below
+    }
+
+    public enum AlarmPriority
+    {
+        High = 1,
+        Medium = 2,
+        Low = 3
+    }
+
+}
