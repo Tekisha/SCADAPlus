e83067a6cbc78a205b9ffe8b06a9bb7f487d504a
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-16 14:18:07 +0200

    feat: implement initial alarm controller

---
+++ SCADA-Core/SCADA-Core/Controllers/interfaces/IAlarmController.cs
@@ -0,0 +1,36 @@
+﻿using SCADA_Core.DTOs;
+using SCADA_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA_Core.Controllers.interfaces
+{
+    [ServiceContract]
+    public interface IAlarmController
+    {
+        [OperationContract]
+        IEnumerable<AlarmDto> GetAll();
+
+        [OperationContract]
+        AlarmDto Get(string alarmName);
+
+        [OperationContract]
+        IEnumerable<AlarmDto> GetInvoked(string tagId, double value);
+
+        [OperationContract]
+        AlarmDto Create(AlarmDto newAlarm);
+
+        [OperationContract]
+        AlarmDto Delete(string alarmName);
+
+        [OperationContract]
+        AlarmDto Update(AlarmDto updatedAlarm);
+
+        [OperationContract]
+        IEnumerable<AlarmDto> GetByTag(string tagId);
+    }
+}
