072f21c0388340697a10224180906636a5159c4d
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-17 02:05:12 +0200

    Added ITrendingService and implementation
    - Introduced `System.ServiceModel` and `Trending.Models` namespaces in `ITrendingService.cs` and `TrendingService.cs`.
    - Created `Trending.Services` namespace and `ITrendingService` interface with `ServiceContract`.
    - Added `SendTagValue` method in `ITrendingService` with `OperationContract` attribute, configured as one-way.
    - Implemented `TrendingService` class, fulfilling `ITrendingService` interface requirements.
    - `SendTagValue` method in `TrendingService` now logs tag details to console.

---
+++ Trending/Services/ITrendingService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+using Trending.Models;
+
+namespace Trending.Services;
+
+[ServiceContract]
+public interface ITrendingService
+{
+    [OperationContract(IsOneWay = true)]
+    void SendTagValue(TagValue value);
+}
