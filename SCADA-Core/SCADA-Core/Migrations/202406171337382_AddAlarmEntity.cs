ac95ac7c993ca14267ab8958b837d8ba1a3cec6e
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-17 15:38:55 +0200

    feat: add alarm entity to databse

---
+++ SCADA-Core/SCADA-Core/Migrations/202406171337382_AddAlarmEntity.cs
@@ -0,0 +1,31 @@
+﻿namespace SCADA_Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAlarmEntity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Alarms",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128, storeType: "nvarchar"),
+                        AlarmName = c.String(nullable: false, maxLength: 256, storeType: "nvarchar"),
+                        TagId = c.String(nullable: false, maxLength: 256, storeType: "nvarchar"),
+                        Limit = c.Double(nullable: false),
+                        Type = c.Int(nullable: false),
+                        Priority = c.Int(nullable: false),
+                        Acknowledged = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Alarms");
+        }
+    }
+}
--- SCADA-Core/SCADA-Core/Models/Alarm.cs
+++ SCADA-Core/SCADA-Core/Models/Alarm.cs
@@ -2,6 +2,7 @@
 using SCADA_Core.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,22 @@ namespace SCADA_Core.Models
 {
     public class Alarm
     {
+        [Key]
+        [Required]
         public string Id { get; set; }
+        [Required]
+        [MaxLength(256)]
         public string AlarmName { get; set; }
+        [Required]
+        [MaxLength(256)]
         public string TagId { get; set; }
+        [Required]
         public double Limit { get; set; }
-        public AlarmType Type { get; set; } 
+        [Required]
+        public AlarmType Type { get; set; }
+        [Required]
         public AlarmPriority Priority { get; set; }
+        [Required]
         public bool Acknowledged { get; set; }
 
         public AlarmDto ToDto()
