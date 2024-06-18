69675382745998f8a9844b50fa0c57565854d29e
MomirMilutinovic <milutinm462@gmail.com>
2024-06-18 12:38:30 -0700

    feat: Add alarm from DatabaseManager

--- DatabaseManager/DatabaseManager/UI/Menu.cs
+++ DatabaseManager/DatabaseManager/UI/Menu.cs
@@ -38,7 +38,8 @@ public class Menu
             { 3, new GetAllTagsCommand(tagServiceProxy) },
             { 4, new TurnScanOnOffCommand(tagServiceProxy) },
             { 5, new LogOutCommand(loggedInAction) },
-            { 6, new ExitCommand(userServiceProxy, tagServiceProxy) }
+            { 6, new AddAlarmCommand(tagServiceProxy) },
+            { 7, new ExitCommand(userServiceProxy, tagServiceProxy) }
         };
 
         _unauthenticatedUserCommands = new Dictionary<int, ICommand>
