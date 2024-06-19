3b22f8cfe88df46d966c411262a2421856414676
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-20 01:21:51 +0200

    SCADA-Core: Refactor DTO classes for clarity and maintenance
    Refactored `AlarmDto.cs`, `TagValueDTO.cs`, and `TriggeredAlarmDto.cs` to enhance code readability and maintainability. Removed unnecessary nested class structures and using directives across these files, streamlining the DTOs for easier understanding and use. Introduced `DataContract` and `DataMember` attributes in `TagValueDTO.cs` and `TriggeredAlarmDto.cs` to improve serialization capabilities, likely in preparation for web service or API integration. Added `ToEntity` and `ToTriggered` methods in `AlarmDto.cs` without altering their functionality, ensuring a seamless transition to a more structured and clean codebase.

--- SCADA-Core/SCADA-Core/DTOs/TagValueDTO.cs
+++ SCADA-Core/SCADA-Core/DTOs/TagValueDTO.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SCADA_Core.DTOs
+namespace SCADA_Core.DTOs;
+
+[DataContract]
+public class TagValueDto
 {
-    [DataContract]
-    public class TagValueDto
-    {
-        [DataMember]
-        public string TagId { get; set; }
-        [DataMember]
-        public string Name { get; set; }
-        [DataMember]
-        public double Value { get; set; }
-        [DataMember]
-        public DateTime Time { get; set; }
-    }
+    [DataMember] public string TagId { get; set; }
+
+    [DataMember] public string Name { get; set; }
+
+    [DataMember] public double Value { get; set; }
+
+    [DataMember] public DateTime Time { get; set; }
 }
--- SCADA-Core/SCADA-Core/DTOs/TriggeredAlarmDto.cs
+++ SCADA-Core/SCADA-Core/DTOs/TriggeredAlarmDto.cs
@@ -1,19 +1,12 @@
-﻿using SCADA_Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
+using SCADA_Core.Models;
 
-namespace SCADA_Core.DTOs
+namespace SCADA_Core.DTOs;
+
+[DataContract]
+public class TriggeredAlarmDto
 {
-    [DataContract]
-    public class TriggeredAlarmDto
-    {
-        [DataMember]
-        public Alarm Alarm { get; set; }
-        [DataMember]
-        public string TagDescription { get; set; }
-    }
+    [DataMember] public Alarm Alarm { get; set; }
+
+    [DataMember] public string TagDescription { get; set; }
 }
