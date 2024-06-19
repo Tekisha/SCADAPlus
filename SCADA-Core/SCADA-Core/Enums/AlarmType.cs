ef022c1650241ddb0767996f52d5fb4ee35f71a5
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-20 01:20:28 +0200

    SCADA-Core: Refactor enums in AlarmPriority and AlarmType
    Refactored `AlarmPriority.cs` and `AlarmType.cs` for better clarity and simplicity. Removed unnecessary using directives and simplified namespace declarations. Also, streamlined enum declarations by removing redundant syntax.

--- SCADA-Core/SCADA-Core/Enums/AlarmType.cs
+++ SCADA-Core/SCADA-Core/Enums/AlarmType.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SCADA_Core.Enums;
 
-namespace SCADA_Core.Enums
+public enum AlarmType
 {
-    public enum AlarmType
-    {
-        Above,
-        Below
-    }
+    Above,
+    Below
 }
