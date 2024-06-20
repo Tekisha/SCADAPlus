2be3a1a954f13849f2538d47bfc387e657a34b11
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-20 02:30:33 +0200

    Database-Manager: Refactor commands and centralize utilities
    - Cleaned up `using` directives in command classes for better namespace management.
    - Changed accessibility of command classes from `internal` to `public`.
    - Centralized common methods (`ReadAlarm`, `GetNonBlankString`) in `Utilities.cs` to adhere to DRY principle.
    - Streamlined `Execute` method implementations in command classes for clarity and efficiency.
    - Simplified `GetDescription` method in command classes for improved readability.
    - Updated `DatabaseManager.csproj` to include `Utilities.cs` in compilation items.

--- DatabaseManager/DatabaseManager/Commands/GetAllAlarmsCommand.cs
+++ DatabaseManager/DatabaseManager/Commands/GetAllAlarmsCommand.cs
@@ -1,29 +1,20 @@
-﻿using DatabaseManager.TagService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using DatabaseManager.TagService;
 
-namespace DatabaseManager.Commands
+namespace DatabaseManager.Commands;
+
+internal class GetAllAlarmsCommand(ITagController tagController) : ICommand
 {
-    internal class GetAllAlarmsCommand(ITagController tagController) : ICommand
+    public string GetDescription()
     {
-        public string GetDescription()
-        {
-            return "View all enabled alarms";
-        }
+        return "View all enabled alarms";
+    }
 
-        public void Execute(string token)
-        {
-
-            Console.WriteLine("All alarms:");
-            Console.WriteLine($"{"Alarm name", -20} | {"Tag ID", -36} | {"Alarm type", -10} | {"Alarm limit", -11} | ");
-            foreach (AlarmDto alarm in tagController.GetAllAlarms(token))
-            {
-                Console.WriteLine($"{alarm.AlarmName, -20} | {alarm.TagId, -36} | {alarm.Type, -10} | {alarm.Limit, 11} | ");
-            }
-        }
-
+    public void Execute(string token)
+    {
+        Console.WriteLine("All alarms:");
+        Console.WriteLine($"{"Alarm name",-20} | {"Tag ID",-36} | {"Alarm type",-10} | {"Alarm limit",-11} | ");
+        foreach (var alarm in tagController.GetAllAlarms(token))
+            Console.WriteLine($"{alarm.AlarmName,-20} | {alarm.TagId,-36} | {alarm.Type,-10} | {alarm.Limit,11} | ");
     }
 }
---
+++ DatabaseManager/DatabaseManager/Utilities.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using DatabaseManager.TagService;
+
+namespace DatabaseManager;
+
+internal class Utilities
+{
+    public static AlarmDto ReadAlarm()
+    {
+        var alarmName = GetNonBlankString("Enter alarm name:", "Alarm name cannot be blank");
+
+        double limit;
+        Console.WriteLine("Enter limit:");
+        while (!double.TryParse(Console.ReadLine(), out limit))
+        {
+            Console.WriteLine("Limit must be a number");
+            Console.WriteLine("Enter limit:");
+        }
+
+        var tagId = GetNonBlankString("Enter tagId:", "Tag id cannot be blank");
+
+
+        int priorityCode;
+        Console.WriteLine("Enter priority (1 - Low | 2 - Medium | 3- High:");
+        while (!int.TryParse(Console.ReadLine(), out priorityCode) || priorityCode < 1 || priorityCode > 3)
+        {
+            Console.WriteLine("Priority must be a number between one and 3");
+            Console.WriteLine("Enter priority:");
+        }
+
+        var priority = (AlarmPriority)priorityCode;
+
+        Console.WriteLine("Enter type (ABOVE/BELOW)");
+        var type = Console.ReadLine();
+        while (type != "ABOVE" && type != "BELOW")
+        {
+            Console.WriteLine("Type must be either ABOVE or BELOW");
+            Console.WriteLine("Enter type (ABOVE/BELOW)");
+            type = Console.ReadLine();
+        }
+
+        var alarmDto = new AlarmDto
+        {
+            AlarmName = alarmName,
+            Limit = limit,
+            Acknowledged = false,
+            TagId = tagId,
+            Priority = priority,
+            Type = type
+        };
+
+        return alarmDto;
+    }
+
+    public static string GetNonBlankString(string prompt, string errorPrompt)
+    {
+        Console.WriteLine(prompt);
+        var value = Console.ReadLine();
+        while (value == null || value.Trim() == "")
+        {
+            Console.WriteLine(errorPrompt);
+            Console.WriteLine(prompt);
+            value = Console.ReadLine();
+        }
+
+        return value;
+    }
+}
