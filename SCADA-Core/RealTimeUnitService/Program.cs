51fad5bcf45a85a53d52370feb20da9300241758
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-06-20 04:35:50 +0200

    Real-Time-Unit: Refactor and enhance service configuration
    - Adjusted namespaces and using directives for WCF support.
    - Improved user input handling with streamlined UI and added null checks.
    - Enhanced validation logic for input values and error messaging.
    - Updated service configuration and initialization for clarity and conciseness.
    - Refined WCF service host configuration with object initializer syntax and debug behavior adjustments.
    - Made UI consistency improvements in error handling and program termination prompts.
    - Applied code formatting and style improvements for better readability.

--- SCADA-Core/RealTimeUnitService/Program.cs
+++ SCADA-Core/RealTimeUnitService/Program.cs
@@ -1,35 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.ComponentModel;
+﻿using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using RealTimeUnit;
 
-
-namespace RealTimeUnit
+namespace RealTimeUnitService
 {
     internal class Program
     {
         private static void Main()
         {
+            Console.Write("Enter port: ");
+            var port = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.Write("Enter min value: ");
+            var minValue = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.Write("Enter max value: ");
+            var maxValue = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
-            Console.WriteLine("Enter port");
-            int port = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter min value");
-            double minValue = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter max value");
-            double maxValue = double.Parse(Console.ReadLine());
-
-            while(maxValue <= minValue)
+            while (maxValue <= minValue)
             {
-                Console.WriteLine("Max value must be greater than min value");
-                Console.WriteLine("Enter min value");
-                minValue = double.Parse(Console.ReadLine());
-                Console.WriteLine("Enter max value");
-                maxValue = double.Parse(Console.ReadLine());
+                Console.WriteLine("ERROR: Max value must be greater than min value");
+                Console.Write("Enter min value: ");
+                minValue = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                Console.Write("Enter max value: ");
+                maxValue = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
             }
 
-            string baseAddress = $"http://localhost:{port}/";
+            var baseAddress = $"http://localhost:{port}/";
             // Create a service collection and configure dependencies
             var serviceCollection = new ServiceCollection();
             ConfigureService(serviceCollection, baseAddress, minValue, maxValue);
@@ -38,21 +35,22 @@ namespace RealTimeUnit
             var serviceProvider = serviceCollection.BuildServiceProvider();
 
 
-
             // Use the service provider to create the WCF service host
             var realTimeUnitServiceHostFactory = new DIServiceHostFactory(serviceProvider);
 
             using (var rtuHost =
                    realTimeUnitServiceHostFactory.CreateServiceHost("RealTimeUnit.RealTimeUnitService",
-                       new Uri[] { new Uri(baseAddress) }))
+                       new[] { new Uri(baseAddress) }))
             {
                 try
                 {
-                    WSDualHttpBinding binding = new WSDualHttpBinding();
-                    binding.SendTimeout = TimeSpan.FromSeconds(10);
-                    binding.ReceiveTimeout = TimeSpan.FromSeconds(10);
-                    binding.OpenTimeout = TimeSpan.FromSeconds(10);
-                    binding.CloseTimeout = TimeSpan.FromSeconds(10);
+                    var binding = new WSDualHttpBinding
+                    {
+                        SendTimeout = TimeSpan.FromSeconds(10),
+                        ReceiveTimeout = TimeSpan.FromSeconds(10),
+                        OpenTimeout = TimeSpan.FromSeconds(10),
+                        CloseTimeout = TimeSpan.FromSeconds(10)
+                    };
                     rtuHost.AddServiceEndpoint("RealTimeUnit.IRealTimeUnitService", binding, "RTU");
                     rtuHost.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
                     rtuHost.Description.Behaviors.Add(
@@ -70,17 +68,18 @@ namespace RealTimeUnit
                     rtuHost.Abort();
                     while (true)
                     {
-                        Console.WriteLine("Press [Enter] to close window.");
+                        Console.Write("Press [Enter] to close window.");
                         if (Console.ReadKey().Key == ConsoleKey.Enter) break;
                     }
                 }
             }
         }
 
-        private static void ConfigureService(IServiceCollection services, string baseAddress, double minValue, double maxValue)
+        private static void ConfigureService(IServiceCollection services, string baseAddress, double minValue,
+            double maxValue)
         {
-            services.AddSingleton(typeof(RealTimeUnitService), new RealTimeUnitService(baseAddress + "RTU", minValue, maxValue));
+            services.AddSingleton(typeof(RealTimeUnit.RealTimeUnitService),
+                new RealTimeUnit.RealTimeUnitService(baseAddress + "RTU", minValue, maxValue));
         }
-
     }
 }
